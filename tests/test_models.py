bd7342d0e34a2ac09aaab8316914ae42226bfab8
mpgh <markus.hundertmark@uni-heidelberg.de>
2024-07-02 17:37:50 +0200

    Add parameterisation mean, min, max test cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -107,3 +107,25 @@ def test_mean_mag(test_df, test_colname, expected):
     """Test mean function works for array of zeroes and positive integers."""
     from lcanalyzer.models import mean_mag
     assert mean_mag(test_df, test_colname) == expected
+
+# Parametrization for mean_mag function testing
+@pytest.mark.parametrize(
+    "test_df, test_colname, expected",
+    [
+        (pd.DataFrame(data=[[1, 5, 3], 
+                            [7, 8, 9], 
+                            [3, 4, 1]], 
+                      columns=list("abc")),
+        "a",
+        pytest.approx(3.66,0.01)),
+        (pd.DataFrame(data=[[0, 0, 0], 
+                            [0, 0, 0], 
+                            [0, 0, 0]], 
+                      columns=list("abc")),
+        "b",
+        0),
+    ])
+def test_mean_mag(test_df, test_colname, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    from lcanalyzer.models import mean_mag
+    assert mean_mag(test_df, test_colname) == expected
